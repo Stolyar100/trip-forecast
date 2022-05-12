0eb47cf965fdb4c0ad66e40aaa04669ecf49d87d
Oleh Marchuk <olehmarchukdev@gmail.com>
2022-05-12 08:56:37 +0300

    Implement own axios instance in api service

--- src/services/owm-service.ts
+++ src/services/owm-service.ts
@@ -1,17 +1,14 @@
-import axios from 'axios';
 import { IOneCallForecast } from '../types';
+import OpwApiCore from '../api-cores/opw-api-core';
 
 class OwpService {
-  baseUrl = 'https://api.openweathermap.org';
-  oneCallPath = '/data/2.5/onecall';
-  ApiKey = '69e708637bee8152dda4ea8f3f1d04d0';
+  private oneCallPath = '/data/2.5/onecall';
 
-  async getForecast (lat:number, lon:number): Promise<IOneCallForecast> {
-    const res = await axios.get(`${this.baseUrl}${this.oneCallPath}`, {
+  async getForecast(lat: number, lon: number): Promise<IOneCallForecast> {
+    const res = await OpwApiCore.get<IOneCallForecast>(this.oneCallPath, {
       params: {
         lat,
         lon,
-        appid: this.ApiKey,
       }
     });
     return res.data;
