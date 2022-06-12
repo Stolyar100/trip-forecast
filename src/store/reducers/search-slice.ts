d373a15663c316a2f0c0e3fa920a310adcef82e7
Oleh Marchuk <olehmarchukdev@gmail.com>
2022-06-12 12:30:37 +0300

    refactored search slice with builder

--- src/store/reducers/search-slice.ts
+++ src/store/reducers/search-slice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IGeocodingIndexed } from '../../types';
-import { fetchSearchCity } from './search-action-creators';
+import { clearSearchResult, fetchSearchCity } from './search-action-creators';
 
 interface SearchState {
   citySearchResult: IGeocodingIndexed[];
@@ -17,33 +17,26 @@ const initialState: SearchState = {
 export const searchSlice = createSlice({
   name: 'search',
   initialState,
-  reducers: {
-    clearSearchResult(state) {
-      state.citySearchResult = [];
-    },
-  },
-  extraReducers: {
-    [fetchSearchCity.fulfilled.type]: (
-      state,
-      action: PayloadAction<IGeocodingIndexed[]>
-    ) => {
-      state.isLoading = false;
-      state.error = '';
-      state.citySearchResult = action.payload;
-    },
-    [fetchSearchCity.pending.type]: (
-      state,
-      action: PayloadAction<IGeocodingIndexed[]>
-    ) => {
-      state.isLoading = true;
-    },
-    [fetchSearchCity.rejected.type]: (state, action: PayloadAction<string>) => {
-      state.isLoading = false;
-      state.error = action.payload;
-      state.citySearchResult = [];
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSearchCity.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = '';
+        state.citySearchResult = action.payload;
+      })
+      .addCase(fetchSearchCity.rejected, (state, action) => {
+        state.isLoading = false;
+        state.citySearchResult = [];
+        typeof action.payload === 'string' && (state.error = action.payload);
+      })
+      .addCase(fetchSearchCity.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(clearSearchResult.fulfilled, (state) => {
+        state.citySearchResult = [];
+      });
   },
 });
 
-export const { clearSearchResult } = searchSlice.actions;
 export default searchSlice.reducer;
